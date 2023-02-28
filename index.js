ec6f2c1e2b94b339bee08320626b701f3b84fa69
KarimB101 <karim.baptiste@yahoo.com>
2023-02-28 14:25:58 -0600

    made prompts for user input?

--- index.js
+++ index.js
@@ -10,8 +10,201 @@ const Intern = require('./lib/Intern')
 const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
 
-// generate html 
+// required to generate html 
 const generateHTML = require("./src/generateHTML");
+const { type } = require('os');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // validate questions to be asked for input from user
+const addRole = () => {
+    return (
+        inquirer.createPromptModule([
+            {
+                name: "Employee-Status",
+                type: "list",
+                message: "Please confirm Employee status you would like to fill out info for.",
+                choices: ["Employee", "Intern", "Manager", "Engineer"],
+            },
 
+        ])
+    )
+};
+
+// function to prompt user through employee questions
+const addEmployee = () => {
+    return (
+        inquirer
+            .createPromptModule([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "What is the Employee's name?",
+                    validate: (name) => {
+                        if (name) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the Employee's Id?",
+                    validate: (id) => {
+                        if (id) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is the Employee's email?",
+                    validate: (email) => {
+                        if (email) { return true; }
+                        else { return false; }
+                    },
+                }
+            ])
+
+    )
+};
+const addManager = () => {
+    return (
+        inquirer
+            .createPromptModule([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "What is the Manager's name?",
+                    validate: (name) => {
+                        if (name) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the Manager's Id?",
+                    validate: (id) => {
+                        if (id) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is the Manager's email?",
+                    validate: (email) => {
+                        if (email) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "Office Number",
+                    type: "input",
+                    message: "What is the Manager's Office Number?",
+                    validate: (officeNumber) => {
+                        if (officeNumber) { return true; }
+                        else { return false; }
+                    },
+                },
+            ])
+
+    )
+};
+const addEngineer = () => {
+    return (
+        inquirer
+            .createPromptModule([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "What is the Engineer's name?",
+                    validate: (name) => {
+                        if (name) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the Engineer's Id?",
+                    validate: (id) => {
+                        if (id) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is the Engineer's email?",
+                    validate: (email) => {
+                        if (email) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "Github Username",
+                    type: "input",
+                    message: "What is the Engineer's Github username?",
+                    validate: (Github) => {
+                        if (Github) { return true; }
+                        else { return false; }
+                    },
+                },
+            ])
+
+    )
+};
+const addIntern = () => {
+    return (
+        inquirer
+            .createPromptModule([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "What is the Intern's name?",
+                    validate: (name) => {
+                        if (name) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the Intern's Id?",
+                    validate: (id) => {
+                        if (id) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is the Intern's email?",
+                    validate: (email) => {
+                        if (email) { return true; }
+                        else { return false; }
+                    },
+                },
+                {
+                    name: "School",
+                    type: "input",
+                    message: "What is the Intern's school?",
+                    validate: (school) => {
+                        if (school) { return true; }
+                        else { return false; }
+                    },
+                },
+            ])
+
+    )
+};
+
+
+// function to import and generate html file with data
+function createTeamPage() {
+    const dom = generateHTML();
+    fs.writeFile("profile.html", dom, function (err) {
+        if (err) throw err;
+        console.log("Profiles created!");
+    });
+};
