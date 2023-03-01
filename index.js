199841ff47ff8b967d3d323fa56034fb60bffa8a
KarimB101 <karim.baptiste@yahoo.com>
2023-03-01 02:09:54 -0600

    pushed answer inputs into array object

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // required to ask questions for user input to generate team profile
 const inquirer = require('inquirer')
 // required to write team profile file
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
+
 
 // employee classes in lib folder
 const Engineer = require("./lib/Engineer");
@@ -15,18 +18,21 @@ const generateHTML = require("./src/generateHTML");
 const { type } = require('os');
 const { default: Choices } = require('inquirer/lib/objects/choices');
 
+// empty array to combine answers for each employee input
+const teamArray = [];
+
 
 
 // validate questions to be asked for input from user
 const addRole = () => {
-        inquirer.prompt([
-            {
-                name: "employeeStatus",
-                type: "list",
-                message: "Please confirm which role you would like to fill out or finish creating team",
-                choices: ["Finish Building Team", "Intern", "Engineer"],
-            },
-        ])
+    inquirer.prompt([
+        {
+            name: "employeeStatus",
+            type: "list",
+            message: "Please confirm which role you would like to fill out next unless done creating team.",
+            choices: ["Finished Building Team", "Intern", "Engineer"],
+        },
+    ])
         .then((answers) => {
             console.log(answers)
             if (answers.employeeStatus === "Engineer") {
@@ -36,7 +42,7 @@ const addRole = () => {
             } else {
                 createTeamPage();
             }
-            // Use user feedback for... whatever!!
+
         })
         .catch((error) => {
             if (error.isTtyError) {
@@ -90,9 +96,10 @@ const addManager = () => {
             },
         ])
         .then((answers) => {
-            console.log(answers)
-            addRole()
-            // Use user feedback for... whatever!!
+            console.log(answers) // user feedback
+            teamArray.push(answers);
+            console.log(teamArray)
+            addRole() // prompts user through employee choices again
         })
         .catch((error) => {
             if (error.isTtyError) {
@@ -105,120 +112,128 @@ const addManager = () => {
 
 };
 const addEngineer = () => {
-        inquirer
-            .prompt([
-                {
-                    name: "name",
-                    type: "input",
-                    message: "What is the Engineer's name?",
-                    validate: (name) => {
-                        if (name) { return true; }
-                        else { return false; }
-                    },
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the Engineer's name?",
+                validate: (name) => {
+                    if (name) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "id",
-                    type: "input",
-                    message: "What is the Engineer's Id?",
-                    validate: (id) => {
-                        if (id) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "What is the Engineer's Id?",
+                validate: (id) => {
+                    if (id) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is the Engineer's email?",
-                    validate: (email) => {
-                        if (email) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is the Engineer's email?",
+                validate: (email) => {
+                    if (email) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "Github",
-                    type: "input",
-                    message: "What is the Engineer's Github username?",
-                    validate: (Github) => {
-                        if (Github) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "Github",
+                type: "input",
+                message: "What is the Engineer's Github username?",
+                validate: (Github) => {
+                    if (Github) { return true; }
+                    else { return false; }
                 },
-            ])
-            .then((answers) => {
-                console.log(answers)
-                // Use user feedback for... whatever!!
-            })
-            .catch((error) => {
-                if (error.isTtyError) {
-                    // Prompt couldn't be rendered in the current environment
-                } else {
-                    // Something else went wrong
-                }
-            });
+            },
+        ])
+        .then((answers) => {
+            console.log(answers)  // user feedback 
+            teamArray.push(answers)
+            console.log(teamArray)
+            addRole() // prompts user through employee choices again
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
 
-    
+
 };
 const addIntern = () => {
-        inquirer
-            .prompt([
-                {
-                    name: "name",
-                    type: "input",
-                    message: "What is the Intern's name?",
-                    validate: (name) => {
-                        if (name) { return true; }
-                        else { return false; }
-                    },
+    inquirer
+        .prompt([
+            {
+                name: "name",
+                type: "input",
+                message: "What is the Intern's name?",
+                validate: (name) => {
+                    if (name) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "id",
-                    type: "input",
-                    message: "What is the Intern's Id?",
-                    validate: (id) => {
-                        if (id) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "What is the Intern's Id?",
+                validate: (id) => {
+                    if (id) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is the Intern's email?",
-                    validate: (email) => {
-                        if (email) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "What is the Intern's email?",
+                validate: (email) => {
+                    if (email) { return true; }
+                    else { return false; }
                 },
-                {
-                    name: "school",
-                    type: "input",
-                    message: "What is the Intern's school?",
-                    validate: (school) => {
-                        if (school) { return true; }
-                        else { return false; }
-                    },
+            },
+            {
+                name: "school",
+                type: "input",
+                message: "What is the Intern's school?",
+                validate: (school) => {
+                    if (school) { return true; }
+                    else { return false; }
                 },
-            ])
-            .then((answers) => {
-                console.log(answers)
-                // Use user feedback for... whatever!!
-            })
-            .catch((error) => {
-                if (error.isTtyError) {
-                    // Prompt couldn't be rendered in the current environment
-                } else {
-                    // Something else went wrong
-                }
-            });
-};
+            },
+        ])
+        .then((answers) => {
+            console.log(answers) // user feedback
+            teamArray.push(answers);
+            console.log(teamArray);
+            addRole() // prompts user through employee choices again
 
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+};
 
 // function to import and generate html file with data
-function createTeamPage() {
-    const dom = generateHTML();
-    fs.writeFile("profile.html", dom, function (err) {
-        if (err) throw err;
-        console.log("Profiles created!");
-    });
-};
+                const createTeamPage = () => {
+                    if (!fs.existsSync(DIST_DIR)) {
+                        fs.mkdirSync(DIST_DIR);
+                      }
+                      fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
+                };
+                // function createTeamPage() {
+                //     const dom = generateHTML();
+                //     fs.writeFile("team.html", dom, function (err) {
+                //         if (err) throw err;
+                //         console.log("Profiles created!");
 
 addManager();
